3b8fe755d8a96127e4c945ab5fed9163fa5e15f5
Cesar Garcia <153330231+AlbrtzxG@users.noreply.github.com>
2024-01-29 15:28:02 +0100

    Update BrakesDistance.py

--- CODE/BrakesDistance.py
+++ CODE/BrakesDistance.py
@@ -2,15 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 ## ////////////////////////////////
-'''
-#Variables by the user
-V0 = int(input("Wich Velocity have the car? ")) / 3.6
-mass = int( input("Wich is the mass? "))
-uFac = float(input("Give me the friction? "))
-'''
 
 #Maths and calculation
-def Distance_Time (Distance_Time_doc, V0, mass, uFac):
+def Distance_Time (V0, mass, uFac):
     #Define variables
     Fr= uFac * 9.81 * mass 
     aceleration= Fr / mass
@@ -22,10 +16,9 @@ def Distance_Time (Distance_Time_doc, V0, mass, uFac):
     Distance = fig2.add_subplot(111)
    
     #data
-    VelTime = V0 * aceleration
     t = np.arange(0.0 , time, 0.1)
     Vel = [V0 - aceleration * timer for timer in t]
-    for vel in reversed(Vel):
+    for vel in Vel:
         Dist += vel * 0.1 
         Distot.append(Dist)
     #Define Plots
@@ -36,9 +29,5 @@ def Distance_Time (Distance_Time_doc, V0, mass, uFac):
     fig2.suptitle("Distance Time of braking", fontweight="bold")
     #print(Distot)
     #print(t)
-    plt.savefig(Distance_Time_doc)
+    plt.savefig("DistanceTime.pdf")
 
-#nombre_archivo_pdf = "DistanceTime.pdf"
-
-# Llamar a la función y exportar la gráfica como PDF
-#Distance_Time(nombre_archivo_pdf)
